0301cd851137b2ce3a8d0de74ca982ef7bdde205
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-01-04 06:46:39 -0500

    Typography.js added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,28 @@ import { Link } from 'gatsby';
 import Header from '../components/header';
 
 export default () => (
-    <div style={{ color: `purple` }}>
-        <Link to="/contact/">Contact</Link> 
-        <Header headerText="Hello Gatsby!" />
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <h1>Richard Hamming on Luck</h1>
+        <div>
+        <p>
+            From Richard Hamming’s classic and must-read talk, “
+            <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+            You and Your Research
+            </a>
+            ”.
+        </p>
+        <blockquote>
+            <p>
+            There is indeed an element of luck, and no, there isn’t. The prepared
+            mind sooner or later finds something important and does it. So yes, it
+            is luck.{" "}
+            <em>
+                The particular thing you do is luck, but that you do something is
+                not.
+            </em>
+            </p>
+        </blockquote>
+        </div>
+        <p>Posted April 09, 2011</p>
     </div>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+
+import lincolnTheme from "typography-theme-lincoln"
+
+const typography = new Typography(lincolnTheme)
+
+export default typography
