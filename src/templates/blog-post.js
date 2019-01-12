cdb35f38c3ec3d36f05154c3d4e88c0f348fc354
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-01-12 09:56:28 -0500

    Disqus shortname configuration fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,7 +5,7 @@ import Disqus from 'disqus-react';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
-  const disqusShortname = 'hands-on.cloud';
+  const disqusShortname = 'hands-on-cloud';
   const disqusConfig = {
     url: post.frontmatter.slug,
     identifier: post.fields.slug,
