3ca8541e9b2229915b9ad526afb5a29477ab8d23
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-01-12 09:32:36 -0500

    Disqus comments added

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
+import Disqus from 'disqus-react';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
+  const disqusShortname = 'hands-on.cloud';
+  const disqusConfig = {
+    url: post.frontmatter.slug,
+    identifier: post.fields.slug,
+    title: post.frontmatter.title,
+  };
+
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
+        <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
+          Comments
+        </Disqus.CommentCount>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
     </Layout>
   );
@@ -20,6 +32,9 @@ export const query = graphql`
       html
       frontmatter {
         title
+      }
+      fields {
+        slug
       }
     }
   }
