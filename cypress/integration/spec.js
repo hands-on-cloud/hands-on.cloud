b4b92d898aeed4b5df71800c41c774268ed66a0b
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-03-02 14:36:37 -0500

    Adding Cypress E2E testing support to CI

---
+++ cypress/integration/spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+describe('Cypress E2E test', function() {
+    beforeEach(() => {
+        // actual url is "baseUrl" in "cypress.json"
+        cy.visit('/')
+    })
+    
+    it('Test main page', function() {
+      cy.visit('/')
+
+      cy.contains('hands-on.cloud')
+
+      cy.get('button[class="mr-2 navbar-toggler"]').click()
+      cy.get('ul.navbar-nav>li').should('have.length', 5) // true
+      cy.contains('AWS')
+      cy.contains('GCP')
+      cy.contains('ML')
+      cy.contains('Terraform')
+      cy.contains('Other')
+    })
+})
