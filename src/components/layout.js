bf8d9a7dd30a3202d533b39433076b24683e0360
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-03-31 19:23:17 -0400

    Revert "Adding GTM"
    This reverts commit 84748916eb7e7ec82d4daea3729643e975b76f80.

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,22 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Layout extends React.Component {
 
     render() {
-        const gtmStyle = {
-            display: 'none',
-            visibility: 'hidden'
-        };
         return (
             <div className="application">
                 <Header />
-                {/* Google Tab Manager */}
-                <script>{`
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-MNPN749');
-                `}</script>
-                {/* Google Tab Manager */}
                 <div className={styles.container}>
                     <Container>
                         <Row>
@@ -41,10 +28,6 @@ export default class Layout extends React.Component {
                         enable_page_level_ads: true
                     });
                 `}</script>
-                {/* Google Tab Manager */}
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNPN749"
-                height="0" width="0" style={gtmStyle} title="GTM"></iframe></noscript>
-                {/* Google Tab Manager */}
             </div>
         )
     }
