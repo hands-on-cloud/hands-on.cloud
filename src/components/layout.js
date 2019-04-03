ba048a89cae0849f75fab185a4d10af72376b829
Andrei Maksimov <maksimov.andrei@gmail.com>
2019-04-02 21:39:47 -0400

    AdSense fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,22 +9,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Layout extends React.Component {
 
     render() {
-        const gtmStyle = {
-            display: 'none',
-            visibility: 'hidden'
-        };
         return (
             <div className="application">
                 <Header />
-                {/* Google Tab Manager */}
-                <script>{`
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-MNPN749');
-                `}</script>
-                {/* Google Tab Manager */}
                 <div className={styles.container}>
                     <Container>
                         <Row>
@@ -36,10 +23,7 @@ export default class Layout extends React.Component {
                 </div>
                 <Footer />
                 <AdSense client="ca-pub-2729052102059896"/>
-                {/* Google Tab Manager */}
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNPN749"
-                height="0" width="0" style={gtmStyle} title="GTM"></iframe></noscript>
-                {/* Google Tab Manager */}
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             </div>
         )
     }
